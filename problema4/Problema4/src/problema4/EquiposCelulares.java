a5ac5e1a3149665ee1ab6f5533350f89c4a51767
JoseMRegalado <mjregalado2@utpl.edu.ec>
2022-04-29 22:16:55 -0500

    manejoclases

--- problema4/Problema4/src/problema4/EquiposCelulares.java
+++ problema4/Problema4/src/problema4/EquiposCelulares.java
@@ -9,7 +9,62 @@ package problema4;
  * @author SALA I
  */
 public class EquiposCelulares {
+    private String sistemaO;
+    private String direccionMac;
+    private double tamanioPantalla;
+    private int informacionImei;
+    private double costoInicial;
+    private double iva;
+    private double ivaCostoInicial;
+    private double costoTotal;
 
+    public void establecerSistemaOperativo(String c){
+        sistemaO = c;
+    }
+    public void establecerDireccionMac(String c){
+        direccionMac = c;
+    }
+    public void establecerTamanioPantalla(double c){
+        tamanioPantalla = c;
+    }
+    public void establecerImei(int c){
+        informacionImei = c;
+    }
+    public void establecerCostoInicial(double c){
+        costoInicial = c;
+    }
+    public void establecerIva(double c){
+        iva = c;
+    }
+    public void calcularIvaCostoInicial(){
+        ivaCostoInicial = (iva / 100)  * costoInicial;
+    }
+    public void calcularCostoTotal(){
+        costoTotal = ivaCostoInicial + costoInicial;
+    }
     
-    
+    public String obtenerSistemaOperativo(){
+        return sistemaO;
+    }
+    public String obtenerDireccionMac(){
+        return direccionMac;
+    }
+    public double obtenerTamanioPantalla(){
+        return tamanioPantalla;
+    }
+    public int obtenerImei(){
+        return informacionImei;
+    }
+    public double obtenerCostoInicial(){
+        return costoInicial;
+    }
+    public double obtenerIva(){
+        return iva;
+    }
+    public double obtenerIvaCostoInicial(){
+        return ivaCostoInicial;
+    }
+    public double obtenerCostoTotal(){
+        return costoTotal;
+    }
 }
--- problema4/Problema4/src/problema4p2/EjecutorEC.java
+++ problema4/Problema4/src/problema4p2/EjecutorEC.java
@@ -3,11 +3,48 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package problema4p2;
+import problema4.EquiposCelulares;
 
-/**
- *
- * @author SALA I
- */
 public class EjecutorEC {
-    
+    public static void main(String[] args) {
+        EquiposCelulares ec = new EquiposCelulares();
+        ec.establecerSistemaOperativo("Android");
+        ec.establecerDireccionMac("4832hj2i9");
+        ec.establecerTamanioPantalla(5);
+        ec.establecerImei(56891547);
+        ec.establecerCostoInicial(450);
+        ec.establecerIva(12);
+        ec.calcularIvaCostoInicial();
+        ec.calcularCostoTotal();
+        System.out.printf("Equipos Celulares\n\nSistema operativo: %s\n"
+                + "Dirección MAC: %s\nTamaño de pantalla: %.2f\n"
+                + "Información IMEI: %d\nIVA: %.2f\n"
+                + "Subtotal: %.2f\nIva del costo inicial: %.2f\n" 
+                + "Costo Total: %.2f", 
+                ec.obtenerSistemaOperativo(),ec.obtenerDireccionMac(),
+                ec.obtenerTamanioPantalla(),ec.obtenerImei(),
+                ec.obtenerIva(),ec.obtenerCostoInicial(),
+                ec.obtenerIvaCostoInicial(),ec.obtenerCostoTotal());
+        System.out.println("");
+        System.out.println("");
+        EquiposCelulares ec2 = new EquiposCelulares();
+        ec2.establecerSistemaOperativo("IOS");
+        ec2.establecerDireccionMac("83629.j92");
+        ec2.establecerTamanioPantalla(7);
+        ec2.establecerImei(3186118);
+        ec2.establecerCostoInicial(220);
+        ec2.establecerIva(14);
+        ec2.calcularIvaCostoInicial();
+        ec2.calcularCostoTotal();
+        System.out.printf("Equipos Celulares\n\nSistema operativo: %s\n"
+                + "Dirección MAC: %s\nTamaño de pantalla: %.2f\n"
+                + "Información IMEI: %d\nIVA: %.2f\n"
+                + "Subtotal: %.2f\nIva del costo inicial: %.2f\n" 
+                + "Costo Total: %.2f", 
+                ec2.obtenerSistemaOperativo(),ec2.obtenerDireccionMac(),
+                ec2.obtenerTamanioPantalla(),ec2.obtenerImei(),
+                ec2.obtenerIva(),ec2.obtenerCostoInicial(),
+                ec2.obtenerIvaCostoInicial(),ec2.obtenerCostoTotal());
+        System.out.println("");
+    }
 }
